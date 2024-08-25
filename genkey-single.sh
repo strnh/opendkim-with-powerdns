a0e5ae21d0b29ff5953d6cf07aad9b878c15568a
strnh <Sotaro.Hikosaka@gmail.com>
2024-08-25 20:00:14 +0900

    Update genkey-single.sh
    for double-quotation problem ..

--- genkey-single.sh
+++ genkey-single.sh
@@ -17,7 +17,7 @@ BASEDOM=example.dom
  --restrict --directory=$KEYDIR
 
 (cd $KEYDIR; cat default.txt | sed "s/[()]//g" | awk 'BEGIN { getline ; sl0=leng
-th($0); s0=substr($0,0,(sl0-1)); getline; sl1=length($0) ; s1=substr($0,5,(sl1-1
+th($0); s0=substr($0,0,(sl0-1)); getline; sl1=length($0) ; s1=substr($0,1,(sl1-1
 )); getline; sl2=length($0); s2=substr($1,5,(sl2)); printf("%s%s%s\n", s0,s1,s2)
  }' > default.txt.pdns
  )
