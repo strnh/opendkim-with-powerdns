57d6782f8b5fdfe0e24d9b65c76743e9821139b2
strnh <Sotaro.Hikosaka@gmail.com>
2024-08-26 13:35:08 +0900

    Update genkey-single.sh
    refine code.

--- genkey-single.sh
+++ genkey-single.sh
@@ -1,24 +1,20 @@
-
 #!/bin/sh
-#
-# opendkim-gen wapper single-domain
-#    for powerdns TXT  with  opendkim-genkey (FreeBSD)
-#
-#
-#  2024/08/25  by @strnh
 # 
+# pdns.sh
+# opendkim-gen wapper 
+# for powerdns TXT  with  opendkim-genkey 
+#
+# 
+#  2024/08/26 13:32 JST by @strnh 
+#  
 #  Tested : FreeBSD/14.1
 
 KEYDIR=/usr/local/etc/mail/keys
 KEYLEN=2048
 BASEDOM=example.dom
+TTL=86400
 
-/usr/local/sbin/opendkim-genkey --note=$BASEDOM --domain=$BASEDOM --bits=$KEYLEN
- --restrict --directory=$KEYDIR
+#/usr/local/sbin/opendkim-genkey --selector=default --note=$BASEDOM --domain=$BASEDOM --bits=$KEYLEN --restrict --directory=$KEYDIR
 
-(cd $KEYDIR; cat default.txt | sed "s/[()]//g" | awk 'BEGIN { getline ; sl0=leng
-th($0); s0=substr($0,0,(sl0-1)); getline; sl1=length($0) ; s1=substr($0,1,(sl1-1
-)); getline; sl2=length($0); s2=substr($1,5,(sl2)); printf("%s%s%s\n", s0,s1,s2)
- }' > default.txt.pdns
+(cd $KEYDIR; cat default.txt | sed "s/\"//g"  | sed "s/\\\//g" |  sed "s/^[ \t]+//g" | sed "s/[()]//g" | awk 'BEGIN { getline ; sl0=length($0); s0=substr($0,0,sl0); getline; sl1=length($0) ;s1=substr($1,0,sl1); getline; sl2=length($0); s2=substr($1,5,(sl2)); printf("%s%s%s\n", s0,s1,s2) }'| awk -v domttl="$BASEDOM $TTL" '{ printf("%s.%s %s %s \"%s %s %s %s %s\" \n",$1,domttl,$2,$3,$4,$5,$6,$7,$8); }' > default.txt.pdns
  )
-
